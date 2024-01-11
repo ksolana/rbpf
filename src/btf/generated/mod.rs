721ae2648f34c4ecf6f6b0f7142bd6774bc1dced
ksolana <110843012+ksolana@users.noreply.github.com>
2024-01-10 23:48:44 -0800

    Add support for aarch64 auto generated files

--- src/btf/generated/mod.rs
+++ src/btf/generated/mod.rs
@@ -9,9 +9,13 @@
 )]
 
 mod btf_internal_bindings;
+#[cfg(target_arch = "aarch64")]
+mod linux_bindings_aarch64;
 #[cfg(target_arch = "x86_64")]
 mod linux_bindings_x86_64;
 
 pub use btf_internal_bindings::{bpf_core_relo, bpf_core_relo_kind, btf_ext_header};
+#[cfg(target_arch = "aarch64")]
+pub use linux_bindings_aarch64::*;
 #[cfg(target_arch = "x86_64")]
 pub use linux_bindings_x86_64::*;
